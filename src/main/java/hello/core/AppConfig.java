735518729ed04b46ffc50addd0409fe73a714e11
JongMinChoi <wonderland314592@gmail.com>
2022-05-21 01:42:48 +0900

    Feat : Sprin gContainer

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,23 +1,42 @@
 package hello.core;
 
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// 생성의 역활(객체의 주입)
+// 구성영역 (생성의 역활(객체의 주입))
+@Configuration
 public class AppConfig {
 
     // 생성자 주입
+    @Bean
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(MemberRepository());
     }
 
     // 생성자 주입
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    @Bean
+    public MemberRepository MemberRepository() {
+        return new MemoryMemberRepository();
     }
 
+    // 생성자 주입
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), discountPolicy());
+    }
+
+    // 생성자 주입
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -1,14 +1,20 @@
 package hello.core;
 
 import hello.core.member.*;
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
+import org.springframework.context.ApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
 //      MemberService memberService = new MemberServiceImpl(new MemoryMemberRepository());
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigReactiveWebApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
