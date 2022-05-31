7628b75f8631d3021e18c451deef097d20b3f935
JongMinChoi <wonderland314592@gmail.com>
2022-05-31 23:27:51 +0900

    Feat : RequestScope

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,14 +1,18 @@
 package hello.core.scope;
 
+import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWithPrototypeTest1 {
+
 
     @Test
     void prototypeFind() {
@@ -22,6 +26,48 @@ public class SingletonWithPrototypeTest1 {
         Assertions.assertThat(prototypeBean2.getCount()).isEqualTo(1);
     }
 
+    @Test
+    void singletonClientPrototype() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+
+        int count1 = clientBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+
+        int count2 = clientBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(1);
+
+
+    }
+
+    @Scope("singleton")
+    @RequiredArgsConstructor
+    static class ClientBean {
+
+        /*
+            ClientBean이 내부에 가지고 있는 프로토 타입 빈은 이미 과거에 주입이 끝난 빈으로서
+            주입 시점에 스프링 컨테이너에 요청해서 프로토타입 빈이 새로 생성된 것이지, 사용 할 때마다
+            새롭게 생성되는 것이 아님
+         */
+
+//        private final PrototypeBean prototypeBean;
+
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
     @Scope("prototype")
     static class PrototypeBean {
         private int count = 0;
@@ -30,7 +76,7 @@ public class SingletonWithPrototypeTest1 {
             count++;
         }
 
-        public int getCount(){
+        public int getCount() {
             return count;
         }
 
